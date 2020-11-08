854ce6b30c5766d4f79398b137706ef5750b5afb
Matteo <matteo.boles@mail.polimi.it>
2020-11-08 12:11:42 +0100

    Inizio stesura codice, definizione macro conteneti indirizzi registri

--- BOLES_MATTEO.cydsn/main.c
+++ BOLES_MATTEO.cydsn/main.c
@@ -11,6 +11,23 @@
 */
 #include "project.h"
 
+// Slave device address
+#define LIS3DH_DEVICE_ADDRESS 0x18
+// Status register address
+#define LIS3DH_STATUS_REG 0x27
+// Control register 1 address
+#define LIS3DH_CTRL_REG1 0x20
+// Control register 4 address
+#define LIS3DH_CTRL_REG4 0x23
+// Address of the accelerometer output on the three axis LSB register
+#define LIS3DH_OUT_XL 0x28
+#define LIS3DH_OUT_YL 0x2A
+#define LIS3DH_OUT_ZL 0x2C
+// Address of the accelerometer output on the three axis MSB register
+#define LIS3DH_OUT_XH 0x29
+#define LIS3DH_OUT_YH 0x2B
+#define LIS3DH_OUT_ZH 0x2D
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
